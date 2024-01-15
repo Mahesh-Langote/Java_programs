34c01f0cf4b24a8549e99a8da142c6bb1de04def
Mahesh Langote <maheshlangote777@gmail.com>
2024-01-15 22:57:59 +0530

    Intial Commit

---
+++ Printing-Pattern/start.java
@@ -0,0 +1,24 @@
+ class start{
+    public static void main(String[] args){
+        int a =5;
+       for (int j=0; j<=a; j++){
+         for(int i=0; i<=a; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+       }
+    }
+ }
+
+ /* output
+ PS D:\Java\Java_programs\Printing-Pattern> javac start.java
+PS D:\Java\Java_programs\Printing-Pattern> java start      
+
+* * * * * * 
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+*/
